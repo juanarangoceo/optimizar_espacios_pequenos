f306e71fab42b7ef76c832a6a7f9150fd082fcb8
juanarangoceo <103976919+juanarangoceo@users.noreply.github.com>
2026-02-09 09:17:58 -0500

    cambios robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://www.optimizarespaciospequenos.com'
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://www.xn--optimizarespaciospequeos-mlc.com'
 
   return {
     rules: {
